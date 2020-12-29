baadd21d90d85d37cb4e4449ed4cc318961c8d54
Michelle Wang <oc3anazns3a@gmail.com>
2020-12-29 01:49:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ function removeFromCart(item) {
   if(foundItem < 0) {
     return "That item is not in your cart.";
   }
-  cart.splice(foundItem, 0);
+  cart = cart.splice(foundItem, 0);
   return cart;
 }
 
