09337ccdb87516fc38d0b43eb6d5cbfe01d6dbb7
Michelle Wang <oc3anazns3a@gmail.com>
2020-12-29 01:27:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -57,4 +57,7 @@ function placeOrder(cardNumber) {
   if(!cardNumber) {
     return "Sorry, we don't have a credit card on file for you.";
   }
+  let total = total();
+  cart.length = 0;
+  return "Your total cost is $" + total + ", which will be charged to the card " + cardNumber + ".";
 }
