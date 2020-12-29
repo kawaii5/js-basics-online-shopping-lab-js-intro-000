d2895380046eb5a4707bfc561affbf3796220038
Michelle Wang <oc3anazns3a@gmail.com>
2020-12-29 01:25:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ function removeFromCart(item) {
   if(foundItem < 0) {
     return "That item is not in your cart.";
   }
-  item.splice(foundItem+1, 1);
+  item.splice(foundItem-1, 1);
 }
 
 function placeOrder(cardNumber) {
