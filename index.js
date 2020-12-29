78bbdd11636c0dd20024ae35f5fcef927ba50c7b
Michelle Wang <oc3anazns3a@gmail.com>
2020-12-29 01:52:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -50,6 +50,7 @@ function removeFromCart(item) {
   if(foundItem < 0) {
     return "That item is not in your cart.";
   }
+  cart[0] = {itemName: "HELLO", itemPrice: Math.floor(Math.random() * 100)}
   cart[1] = {itemName: "HELLO", itemPrice: Math.floor(Math.random() * 100)}
 }
 
